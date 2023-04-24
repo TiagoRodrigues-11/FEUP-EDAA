f3ee71920ce2146e849af76fa00872c80bdfa7c8
Diogo Faria <dluis34@gmail.com>
2023-04-24 14:10:33 +0100

    added library for c++ postgresql connection

---
+++ src/KdTree/CMakeFiles/KdTree.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/KdTree.dir/Song.cpp.o"
+  "CMakeFiles/KdTree.dir/Song.cpp.o.d"
+  "CMakeFiles/KdTree.dir/main.cpp.o"
+  "CMakeFiles/KdTree.dir/main.cpp.o.d"
+  "KdTree"
+  "KdTree.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/KdTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ src/KdTree/CMakeFiles/KdTree.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for KdTree.
+# This may be replaced when dependencies are built.
