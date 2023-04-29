0d2fb4b79139446586bb97b7dcf99fd7d3545725
André Flores <andrejesusferflores@gmail.com>
2023-04-29 23:11:31 +0100

    Update docker image db

--- src/docker_images/create_postgres_sql_file.sh
+++ src/docker_images/create_postgres_sql_file.sh
@@ -9,14 +9,13 @@ rm -f spotify-postgres.sql
 echo "Creating new sqlite dump..."
 sqlite3 ../data/spotify.sqlite .dump > spotify.sql
 echo "Converting to utf-8..."
-iconv -f utf-16le -t utf-8 spotify.sql > spotify-utf-8.sql
+iconv -f latin1 -t utf-8 spotify.sql > spotify-utf-8.sql
 rm -f spotify.sql
 echo "Removing pragma..."
-sed -i '1d' spotify-utf-8.sql > spotify-utf-8-no-pragma.sql
-rm -f spotify-utf-8.sql
+sed -i '1d' spotify-utf-8.sql
 echo "Extracting data..."
-grep 'INSERT INTO' spotify-utf-8-no-pragma.sql > spotify-data.sql
-rm -f spotify-utf-8-no-pragma.sql
+grep 'INSERT INTO' spotify-utf-8.sql > spotify-data.sql
+rm -f spotify-utf-8.sql
 echo "Creating new database sql file..."
 cat begin-transaction.sql schema.sql spotify-data.sql create-tracks-table-and-end-transaction.sql > spotify-postgres.sql
 rm -f spotify-data.sql
