dbd2a3668d4970c7915eecd5ed68f919fa3e8d00
André Flores <andrejesusferflores@gmail.com>
2023-05-18 18:14:11 +0100

    Remove vector splitting prints

--- src/KdTree/KdTree.hpp
+++ src/KdTree/KdTree.hpp
@@ -390,8 +390,6 @@ void KdTree<Point>::buildTree(std::vector<Point*> &points, KdTreeNode<Point>* &n
 
     std::chrono::duration<double> elapsed_seconds = end - start;
 
-    std::cout << "Splitting time(s):" << elapsed_seconds.count() << std::endl;
-
     // Store the time taken to split
     sort_time[thread_no] += ((double)elapsed_seconds.count());
 
